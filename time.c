a02a3c92922da260bdd80a08c09acae46515c0c9
Sid <siddharthmethiya180@gmail.com>
2025-09-27 22:14:45 +0530

    added header for time_server

--- time.c
+++ time.c
@@ -3,6 +3,7 @@
 
 int main() {
     time_t timer;
+    // This function sets the current time from local in timer
     time(&timer);
 
     printf("Your local time is: %s", ctime(&timer));
---
+++ time_server.c
@@ -0,0 +1,17 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+
+#define ISVALIDSOCKET(s) ((s) >= 0)
+#define CLOSESOCKET(s) close(s)
+#define SOCKET int
+#define GETSOCKETERRNO() (errno)
+
+int main() { return 0; }
