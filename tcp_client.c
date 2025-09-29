49afc939374daa3f014990fdd99697e2d5dfcf85
Sid <siddharthmethiya180@gmail.com>
2025-09-30 00:20:57 +0530

    Done creating tcpClient

---
+++ tcp_client.c
@@ -0,0 +1,103 @@
+#include <arpa/inet.h>
+#include <bits/types/struct_timeval.h>
+#include <errno.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+#define ISVALIDSOCKET(s) ((s) >= 0)
+#define CLOSESOCKET(s) close(s)
+#define SOCKET int
+#define GETSOCKETERRNO() (errno)
+
+int main(int argc, char* argv[]) {
+    if (argc < 3) {
+        fprintf(stderr, "Usage: tcp_client_hostname port \n");
+        return 1;
+    }
+    printf("Configuring local address...\n");
+    // This is used to create a struct named hints which has all addrinfo content
+    struct addrinfo hints;
+    struct addrinfo* bind_addresses;
+    // This function sets members inside hints to 0
+    memset(&hints, 0, sizeof(hints));
+
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_family = AF_INET;
+    struct addrinfo* peer_address;
+    if (getaddrinfo(argv[1], argv[2], &hints, &peer_address)) {
+        fprintf(stderr, "getaddrinfo() failed. (%d)\n", GETSOCKETERRNO());
+        return 1;
+    }
+
+    printf("Remote address is:\n");
+    char address_buffer[100];
+    char service_buffer[100];
+    getnameinfo(peer_address->ai_addr, peer_address->ai_addrlen, address_buffer, sizeof(address_buffer), service_buffer, sizeof(service_buffer), NI_NUMERICHOST);
+
+    printf("%s %s\n", address_buffer, service_buffer);
+
+    printf("Creating socket: \n");
+    SOCKET socket_peer;
+    socket_peer = socket(peer_address->ai_family, peer_address->ai_socktype, peer_address->ai_protocol);
+
+    if (!ISVALIDSOCKET(socket_peer)) {
+        fprintf(stderr, "Creating socker failed...(%d)\n", GETSOCKETERRNO());
+        return 1;
+    }
+
+    printf("Connecting...\n");
+    if (connect(socket_peer, peer_address->ai_addr, peer_address->ai_addrlen)) {
+        fprintf(stderr, "Connecting socket failed...(%d)\n", GETSOCKETERRNO());
+        return 1;
+    }
+
+    freeaddrinfo(peer_address);
+    printf("Connected.\n");
+    printf("To send data, enter text followed by enter.\n");
+
+    while (1) {
+        fd_set reads;
+        FD_ZERO(&reads);
+        FD_SET(0, &reads);
+        FD_SET(socket_peer, &reads);
+
+        struct timeval timeout;
+        timeout.tv_sec = 0;
+        timeout.tv_usec = 100000;
+
+        if (select(socket_peer + 1, &reads, 0, 0, &timeout) < 0) {
+            fprintf(stderr, "Select () failed...(%d)\n", GETSOCKETERRNO());
+            return 1;
+        }
+
+        if (FD_ISSET(socket_peer, &reads)) {
+            char read[4096];
+            int bytes_received = recv(socket_peer, read, 4096, 0);
+            if (bytes_received < 1) {
+                printf("Connection closed by peer....");
+                break;
+            }
+            printf("Received (%d bytes): %.*s", bytes_received, bytes_received, read);
+        }
+
+        if (FD_ISSET(0, &reads)) {
+            char read[4096];
+            if (!fgets(read, 4096, stdin)) break;
+
+            printf("Sending: %s", read);
+            int bytes_sent = send(socket_peer, read, strlen(read), 0);
+            printf("Sent %d bytes.\n", bytes_sent);
+        }
+    }
+    printf("Closing socket...\n");
+    CLOSESOCKET(socket_peer);
+
+    printf("Finsihed...\n");
+    return 0;
+}
